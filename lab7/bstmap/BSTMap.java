e2ceb64294cda0a8a1563d31e34b62059e709f8f
Kate Fang <fm980513@126.com>
2022-10-27 05:28:01 -0600

    Done with lab 7

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -1,113 +1,170 @@
 package bstmap;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
-public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>{
-    private class Node {
-        private K key;           // sorted by key
-        private V val;         // associated data
-        private Node left, right;  // left and right subtrees
-        private int size;          // number of nodes in subtree
-
-        public Node(K key, V val) {
-            this.key = key;
-            this.val = val;
-        }
-    }
-
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     private Node root;
-    private int size;
+    private int size = 0;
 
-    public BSTMap() {
-        this. root = null;
-        this.size = 0;
-    }
-
-    public BSTMap(K key, V val) {
-        this.root = new Node(key, val);
-        this.size = 1;
-    }
-
-    @Override
     public void clear() {
-        this.root = null;
-        this.size = 0;
+        root = null;
+        size = 0;
     }
 
-    @Override
     public boolean containsKey(K key) {
-        if (key == null) throw new IllegalArgumentException();
-        if (this == null) return false;
-
         return containsKey(root, key);
     }
 
-    public boolean containsKey(Node root, K key) {
-        if (root == null) return false;
-        int cmp = key.compareTo(root.key);
-        if (cmp > 0) return containsKey(root.right, key);
-        else if (cmp < 0) return containsKey(root.left, key);
-        else return true;
+    private boolean containsKey(Node node, K key) {
+        if (node == null) {
+            return false;
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            return containsKey(node.left, key);
+        } else if (cmp > 0) {
+            return containsKey(node.right, key);
+        }
+        return true;
     }
 
-    @Override
     public V get(K key) {
-        if (key == null) throw new IllegalArgumentException();
-        if (this == null) return null;
         return get(root, key);
     }
 
-    public V get(Node root, K key) {
-        if (root == null) return null;
-        int cmp = key.compareTo(root.key);
-        if (cmp > 0) return get(root.right, key);
-        else if (cmp < 0) return get(root.left, key);
-        else return root.val;
-    }
-
-    @Override
-    public int size() {
-        return this.size;
-    }
-
-    @Override
-    public void put(K key, V value) {
-        put(this.root, key, value);
-    }
-
-    public Node put(Node root, K key, V value) {
-        if (root == null) {
-            root = new Node(key, value);
+    private V get(Node node, K key) {
+        if (node == null) {
+            return null;
         }
-        int cmp = key.compareTo(root.key);
+        int cmp = key.compareTo(node.key);
         if (cmp < 0) {
-            root.left = put(root.left, key, value);
+            return get(node.left, key);
         } else if (cmp > 0) {
-            root.right = put(root.right, key, value);
-        } else {
-            root.val = value;
+            return get(node.right, key);
         }
-        return root;
+        return node.value;
     }
 
-    @Override
-    public Set keySet() {
-        return null;
+    public int size() {
+        return size;
     }
 
-    @Override
+    public void put(K key, V value) {
+        root = put(root, key, value);
+        size += 1;
+    }
+
+    private Node put(Node node, K key, V value) {
+        if (node == null) {
+            return new Node(key, value);
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = put(node.left, key, value);
+        } else if (cmp > 0) {
+            node.right = put(node.right, key, value);
+        } else {
+            node.value = value;
+        }
+        return node;
+    }
+
+    public Set<K> keySet() {
+        HashSet<K> set = new HashSet<>();
+        addKeys(root, set);
+        return set;
+    }
+
+    private void addKeys(Node node, Set<K> set) {
+        if (node == null) {
+            return;
+        }
+        set.add(node.key);
+        addKeys(node.left, set);
+        addKeys(node.right, set);
+    }
+
     public V remove(K key) {
+        if (containsKey(key)) {
+            V targetValue = get(key);
+            root = remove(root, key);
+            size -= 1;
+            return targetValue;
+        }
         return null;
     }
 
-    @Override
+    private Node remove(Node node, K key) {
+        if (node == null) {
+            return null;
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = remove(node.left, key);
+        } else if (cmp > 0) {
+            node.right = remove(node.right, key);
+        } else {
+            if (node.left == null) {
+                return node.right;
+            }
+            if (node.right == null) {
+                return node.left;
+            }
+            Node originalNode = node;
+            node = getMinChild(node.right);
+            node.left = originalNode.left;
+            node.right = remove(originalNode.right, node.key);
+        }
+        return node;
+    }
+
+    private Node getMinChild(Node node) {
+        if (node.left == null) {
+            return node;
+        }
+        return getMinChild(node.left);
+    }
+
     public V remove(K key, V value) {
+        if (containsKey(key)) {
+            V targetValue = get(key);
+            if (targetValue.equals(value)) {
+                root = remove(root, key);
+                size -= 1;
+                return targetValue;
+            }
+        }
         return null;
     }
 
-    @Override
-    public Iterator iterator() {
-        return null;
+    public Iterator<K> iterator() {
+        return keySet().iterator();
+    }
+
+    public void printInOrder() {
+        printInOrder(root);
+    }
+
+    private void printInOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        printInOrder(node.left);
+        System.out.println(node.key.toString() + " -> " + node.value.toString());
+        printInOrder(node.right);
+    }
+
+    private class Node {
+        public final K key;
+        public V value;
+        public Node left;
+        public Node right;
+
+        public Node(K k, V v) {
+            key = k;
+            value = v;
+        }
     }
 }
